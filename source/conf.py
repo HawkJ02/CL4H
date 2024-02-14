65f228a02d4e7d42485aa2b911547c944d725d95
HawkJ <9678126+li-haojin@user.noreply.gitee.com>
2024-02-14 09:55:18 +0800

    first commit

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Code Library From HawkJ'
+copyright = '2024, www.hawkjgogogo.com'
+author = 'HawkJ'
+release = 'v1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+#extensions = []
+extensions = ['recommonmark','sphinx_markdown_tables']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'groundwork'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# The master toctree document.
+master_doc = 'index'
---
+++ source/index.rst
@@ -0,0 +1,21 @@
+.. test documentation master file, created by
+   sphinx-quickstart on Tue Feb 13 20:11:12 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+www.hawkjgogogo.com
+================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+   代码模块/index
+
+Indices and tables
+
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
